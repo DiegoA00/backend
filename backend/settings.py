59d410211eca93bbc01ce6f81a9c807b2478c4d3
DiegoA00 <dienarau@espol.edu.ec>
2025-01-08 10:23:00 -0500

    Add connection to db and GET and POST methods

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import firebase_admin
+from firebase_admin import credentials
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
+    "rest_framework",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CRED = credentials.Certificate(
+    "keys/landing-c1529-firebase-adminsdk-780qw-05edcae050.json"
+)
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(
+    FIREBASE_CRED, {"databaseURL": "https://landing-c1529-default-rtdb.firebaseio.com/"}
+)
