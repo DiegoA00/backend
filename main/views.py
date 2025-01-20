95d0f455c70a681ba24c33b2eb4f65dd8e586716
DiegoA00 <dienarau@espol.edu.ec>
2025-01-20 13:29:37 -0500

    Add table rows and change linechart

--- main/views.py
+++ main/views.py
@@ -4,17 +4,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Importe el decorador login_required
-from django.contrib.auth.decorators import login_required,  permission_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Importe requests y json
 import requests
 import json
-import datetime
+from datetime import datetime
+from collections import Counter
 
 
 # Restricción de acceso con @login_required
 @login_required
-@permission_required('main.index_viewer', raise_exception=True)
+@permission_required("main.index_viewer", raise_exception=True)
 def index(request):
 
     # Arme el endpoint del REST API
@@ -25,42 +26,63 @@ def index(request):
     response_http = requests.get(url)
     response_dict = json.loads(response_http.content)
 
-    # print("Endpoint ", url)
-    # print("Response ", response_dict)
-
     # Respuestas totales
     total_responses = len(response_dict.keys())
 
-    # for key, value in response_dict.items():
-    #     fecha_str = value.get("saved")
+    first_response = None
+    last_response = None
+    date_counts = Counter()
 
-    #     if fecha_str is None:
-    #         continue  # Si no hay fecha, pasa a la siguiente iteración
+    for key, value in response_dict.items():
+        fecha_str = value.get("saved")
+        if not fecha_str:
+            continue
 
-    #     fecha_str = fecha_str.replace("p.\xa0m.", "PM").replace("a.\xa0m.", "AM")
+        fecha_str = fecha_str.replace("\xa0", " ")
+        try:
+            fecha_datetime = datetime.strptime(fecha_str, "%d/%m/%Y, %I:%M:%S %p")
 
-    #     try:
-    #         fecha_datetime = datetime.strptime(fecha_str, "%d/%m/%Y, %I:%M:%S %p")
-    #     except ValueError:
-    #         # Manejo para fechas con un solo dígito en el día o mes
-    #         fecha_datetime = datetime.strptime(fecha_str, "%d/%m/%Y, %I:%M:%S %p")
+            if first_response is None or fecha_datetime < first_response:
+                first_response = fecha_datetime
 
-    #     print(f"Fecha procesada: {fecha_datetime}")
+            if last_response is None or fecha_datetime > last_response:
+                last_response = fecha_datetime
 
-    #     # Comparación para encontrar la fecha más antigua
-    #     if first_response is None or fecha_datetime < first_response:
-    #         first_response = fecha_datetime
+            # Contar las fechas por día
+            date_counts[fecha_datetime.date()] += 1
 
+        except ValueError as e:
+            print(f"Error al procesar la fecha '{fecha_str}': {e}")
+
+    print(f"Fecha más antigua encontrada: {first_response}")
+    print(f"Fecha más reciente encontrada: {last_response}")
+
+    # Obtener el día con más respuestas
+    most_common_day, most_common_count = (
+        date_counts.most_common(1)[0] if date_counts else (None, 0)
+    )
+
+    # Formatear fechas a short date (DD/MM/YYYY)
+    first_response_str = first_response.strftime("%d/%m/%Y") if first_response else None
+    last_response_str = last_response.strftime("%d/%m/%Y") if last_response else None
+    most_common_day_str = (
+        most_common_day.strftime("%d/%m/%Y") if most_common_day else None
+    )
 
     # Valores de la respuesta
-    # responses = response_dict.values()
+    responses = response_dict.values()
 
     # Objeto con los datos a renderizar
     data = {
-        "title": "Landing - Dashboard",
+        "title": "MotorCity - Dashboard",
         "total_responses": total_responses,
-        # "first_response": first_response,
-        # "responses": responses,
+        "first_response": first_response_str,
+        "last_response": last_response_str,
+        "high_rate_responses": str(most_common_day_str)
+        + " ("
+        + str(most_common_count)
+        + " respuestas)",
+        "responses": responses,
     }
 
     # Renderización en la plantilla
--- static/js/charts-lines.js
+++ static/js/charts-lines.js
@@ -4,38 +4,26 @@
 const lineConfig = {
   type: 'line',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: [],
     datasets: [
       {
-        label: 'Organic',
-        /**
-         * These colors come from Tailwind CSS palette
-         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-         */
+        label: 'Contado',
         backgroundColor: '#0694a2',
         borderColor: '#0694a2',
-        data: [43, 48, 40, 54, 67, 73, 70],
+        data: [],
         fill: false,
       },
       {
-        label: 'Paid',
+        label: 'Crédito',
         fill: false,
-        /**
-         * These colors come from Tailwind CSS palette
-         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-         */
         backgroundColor: '#7e3af2',
         borderColor: '#7e3af2',
-        data: [24, 50, 64, 74, 52, 51, 65],
+        data: [],
       },
     ],
   },
   options: {
     responsive: true,
-    /**
-     * Default legends are ugly and impossible to style.
-     * See examples in charts.html to add your own legends
-     *  */
     legend: {
       display: false,
     },
@@ -52,14 +40,14 @@ const lineConfig = {
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Month',
+          labelString: '',
         },
       },
       y: {
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Value',
+          labelString: '',
         },
       },
     },
@@ -69,3 +57,61 @@ const lineConfig = {
 // change this to the id of your chart element in HMTL
 const lineCtx = document.getElementById('line')
 window.myLine = new Chart(lineCtx, lineConfig)
+
+countResponsesPerDay = (data) => {
+  const labels = [];
+  for (let i = 6; i >= 0; i--) {
+    const date = new Date();
+    date.setDate(date.getDate() - i);
+    labels.push(date.toLocaleDateString('es-ES', { weekday: 'long' }));
+  }
+  const countsContado = [0, 0, 0, 0, 0, 0, 0];
+  const countsCredito = [0, 0, 0, 0, 0, 0, 0];
+
+  Object.values(data).forEach(record => {
+    const savedTime = record.saved;
+    const paymentType = record.pago;
+    if (!savedTime || !paymentType) {
+      return;
+    }
+
+    const formattedTime = savedTime.replace('a. m.', 'AM').replace('p. m.', 'PM');
+    const dt = new Date(Date.parse(formattedTime.replace(/(\d{2}\/\d{2}\/\d{4}), (\d{2}):(\d{2}):(\d{2}) (AM|PM)/, '$1 $2:$3:$4 $5')));
+    const day = dt.getDay();
+
+    if (paymentType === 'contado') {
+      countsContado[day]++;
+    } else if (paymentType === 'crédito') {
+      countsCredito[day]++;
+    }
+  });
+
+  return { labels, countsContado, countsCredito };
+}
+
+update = () => {
+  fetch('/api/v1/landing')
+    .then(response => response.json())
+    .then(data => {
+      let { labels, countsContado, countsCredito } = countResponsesPerDay(data)
+
+      // Reset data
+      window.myLine.data.labels = [];
+      window.myLine.data.datasets[0].data = [];
+      window.myLine.data.datasets[1].data = [];
+
+      // New data
+      window.myLine.data.labels = [...labels]
+      window.myLine.data.datasets[0].data = [...countsContado]
+      window.myLine.data.datasets[1].data = [...countsCredito]
+
+      // Update axis labels
+      window.myLine.options.scales.x.scaleLabel.labelString = 'Día de la semana';
+      window.myLine.options.scales.y.scaleLabel.labelString = 'Cantidad';
+
+      window.myLine.update();
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+update();
