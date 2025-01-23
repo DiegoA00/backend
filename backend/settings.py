950ce9ff4a67003a4815b8a35b130d5358fac30d
DiegoA00 <dienarau@espol.edu.ec>
2025-01-22 22:25:14 -0500

    add procfile

--- backend/settings.py
+++ backend/settings.py
@@ -118,7 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Configuración adicional de archivos estáticos para Railway
 STATICFILES_DIRS = [BASE_DIR / "static"]
